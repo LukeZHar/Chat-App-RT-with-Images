c62f2c22b8b3935859dd40635f2b5fdb341ce0e3
LukeZHar <luke.z.harris@hotmail.com>
2025-11-05 19:51:20 +1000

    feat: enhance SignUpPage with form validation and loading state; add AuthImagePattern component fix: update ESLint config to disable prop-types rule

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   useEffect(() => {
     checkAuth()
   }, [checkAuth])
-  console.log('Auth User:', authUser);
+  console.log({ authUser });
 
   if (isCheckingAuth && !authUser) return (
     <div className='flex items-center justify-center h-screen'>
@@ -30,7 +30,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
