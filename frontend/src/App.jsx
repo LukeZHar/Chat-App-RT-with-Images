92683b8542d0ad4a445978d81f0029303d473930
LukeZHar <luke.z.harris@hotmail.com>
2025-11-05 14:36:45 +1000

    feat: integrate Tailwind CSS and set up routing
    - Added Tailwind CSS and DaisyUI to the project.
    - Created a PostCSS configuration file.
    - Removed App.css and replaced it with Tailwind's utility classes.
    - Implemented a Navbar component for navigation.
    - Set up routing with React Router for Home, Sign Up, Login, Settings, and Profile pages.
    - Created placeholder pages for Home, Sign Up, Login, Settings, and Profile.
    - Added Zustand for state management to handle authentication.
    - Created an Axios instance for API requests.
    - Updated main entry point to include BrowserRouter.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar.jsx'
+import HomePage from './pages/HomePage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SettingsPage from './pages/SettingsPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Route, Routes } from 'react-router-dom'
+import { useAuthStore } from './store/useAuthStore.js'
+import { useEffect } from 'react'
+
+const App = () => {
+  const { authUser, checkAuth } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+  console.log('Auth User:', authUser)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
   )
 }
 
