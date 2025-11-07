e10ee31c96db09c1c4072d86191dd6e758da4ccf
LukeZHar <luke.z.harris@hotmail.com>
2025-11-07 22:43:09 +1000

    fix: correct variable name for upload response in updateProfile function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -90,7 +90,7 @@ export const updateProfile = async (req, res) => {
       return res.status(400).json({ message: "Profile picture is required." });
     }
 
-    const updateResponse = await cloudinary.uploader.upload(profilePic);
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
     const updatedUser = await User.findByIdAndUpdate(
       userId,
       { profilePic: uploadResponse.secure_url },
