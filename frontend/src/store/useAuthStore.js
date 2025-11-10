93506669cd8786b1f38e5abab70c18e3cf9e169a
LukeZHar <luke.z.harris@hotmail.com>
2025-11-10 12:13:43 +1000

    feat: update backend configuration and add production support for static files

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,8 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -97,8 +98,8 @@ export const useAuthStore = create((set, get) => ({
 
     const socket = io(BASE_URL, {
       query: {
-        userId: authUser._id
-      }
+        userId: authUser._id,
+      },
     });
     socket.connect();
     set({ socket: socket });
